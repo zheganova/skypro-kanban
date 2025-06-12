c210ca03e8e25039707ebb13ef9647fb1f741438
Полина <zeganovapolina@gmail.com>
2025-06-13 00:18:05 +0400

    Показываем модальное окно popuser по клику на имя пользователя

--- src/components/PopUser/PopUser.jsx
+++ src/components/PopUser/PopUser.jsx
@@ -1,6 +1,14 @@
-export const PopUser = () => {
+export const PopUser = ({ isVisible, toggleVisibility }) => {
+  // Принимаем пропсы isVisible и toggleVisibility
+  const popUserStyle = {
+    display: isVisible ? "block" : "none",
+  };
   return (
-    <div className="header__pop-user-set pop-user-set" id="user-set-target">
+    <div
+      className="header__pop-user-set pop-user-set"
+      id="user-set-target"
+      style={popUserStyle}
+    >
       <p className="pop-user-set__name">Ivan Ivanov</p>
       <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
       <div className="pop-user-set__theme">
@@ -8,7 +16,11 @@ export const PopUser = () => {
         <input type="checkbox" className="checkbox" name="checkbox" />
       </div>
       <button type="button" className="_hover03">
-        <a href="#popExit">Выйти</a>
+        {/* При клике на "Выйти" также закрываем PopUser, если нужно, или переходим по ссылке */}
+        {/* В данном случае, чтобы закрыть окно при клике на "Выйти", можно вызвать toggleVisibility */}
+        <a href="#popExit" onClick={toggleVisibility}>
+          Выйти
+        </a>
       </button>
     </div>
   );

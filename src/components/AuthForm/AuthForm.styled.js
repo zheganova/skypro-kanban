13269274227c81b48cc9879ea182df7f9a9818d2
Полина <zeganovapolina@gmail.com>
2025-07-11 15:10:05 +0400

    Попытка изменить стили для форм входа и регистрации.

--- src/components/AuthForm/AuthForm.styled.js
+++ src/components/AuthForm/AuthForm.styled.js
@@ -8,20 +8,14 @@ export const AuthBg = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: rgba(
-    9,
-    30,
-    63,
-    0.3
-  ); 
+  background: rgba(9, 30, 63, 0.3);
 `;
 
 export const AuthModal = styled.div`
   width: 368px;
-  height: auto;
+  height: 329px;
   background-color: #ffffff;
   border-radius: 10px;
-  padding: 43px 47px 47px 40px;
   position: relative;
   box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.1);
 
@@ -34,6 +28,7 @@ export const AuthModal = styled.div`
 export const AuthWrapper = styled.div`
   /* Wrapper, который содержит заголовок и форму */
   width: 100%;
+  // margin: 50px 60px 50px 60px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -41,11 +36,11 @@ export const AuthWrapper = styled.div`
 
 export const AuthTitle = styled.h2`
   color: #000000;
-  font-size: 20px;
+  font-family: Roboto;
   font-weight: 700;
-  line-height: 30px;
-  letter-spacing: -0.4px;
-  margin-bottom: 30px;
+  font-size: 20px;
+  line-height: 150%;
+  letter-spacing: -0.03em;
   text-align: center;
 `;
 
@@ -58,23 +53,25 @@ export const AuthFormstyle = styled.form`
 `;
 
 export const InputWrapper = styled.div`
-  width: 100%;
+  width: 248px;
   display: flex;
   flex-direction: column;
-  gap: 14px; /* Отступ между инпутами */
+  gap: 7px; /* Отступ между инпутами */
   margin-bottom: 20px; /* Отступ после группы инпутов перед кнопкой */
 `;
 
 export const AuthInput = styled.input`
   width: 100%;
   height: 30px;
-  border-radius: 4px;
+  border-radius: 8px;
   border: 0.7px solid rgba(148, 166, 190, 0.4);
   outline: none;
-  padding: 8px 10px;
+  padding: 4.5px 10px;
+  font-family: Roboto;
+  font-weight: 400;
   font-size: 14px;
-  line-height: 21px;
-  letter-spacing: -0.14px;
+  line-height: 150%;
+  letter-spacing: -0.02em;
 
   &::placeholder {
     color: #94a6be;
@@ -82,7 +79,7 @@ export const AuthInput = styled.input`
 `;
 
 export const ButtonEnter = styled.button`
-  width: 100%;
+  width: 248px;
   height: 30px;
   background-color: #565eef;
   border-radius: 4px;
@@ -94,7 +91,6 @@ export const ButtonEnter = styled.button`
   border: none;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
-  margin-top: 20px; /* Отступ сверху от формы */
 
   &:hover {
     background-color: #33399b;
@@ -103,25 +99,30 @@ export const ButtonEnter = styled.button`
 
 export const FormGroup = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
   gap: 5px;
 
   p {
-    color: #94A6BE66;
-    font-size: 14px;
+    color: #94a6be66;
     line-height: 21px;
     letter-spacing: -0.14px;
+    font-family: Roboto;
+    font-weight: 400;
+    font-size: 14px;
   }
 
   a {
-    color: #94A6BE66;
-    font-size: 14px;
+    color: #94a6be66;
     line-height: 21px;
     letter-spacing: -0.14px;
     text-decoration: underline;
     cursor: pointer;
+    font-family: Roboto;
+    font-weight: 400;
+    font-size: 14px;
 
     &:hover {
       color: #565eef;
